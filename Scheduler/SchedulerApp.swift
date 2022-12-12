af1a03c5f8ca34c1d117c079b75dd588e0ec22a6
rhysdh540 <rhys_dehaan@ryecountryday.org>
2022-12-12 14:03:52 -0500

    Initial Commit

---
+++ Scheduler/SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerApp.swift
+//  Scheduler
+//
+//  Created by Rhys de Haan on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
